4236a036c3a667ec598fd0c56924a8649c85c020
Turjo99 <imturjo27@gmail.com>
2023-03-23 11:39:29 +0600

    login page and out created

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,18 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Error from "../../Pages/Error/Error";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home/>
-            },
-            
-
-        ],
-        errorElement: <Error></Error>
-    }
-])
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+    errorElement: <Error></Error>,
+  },
+]);
