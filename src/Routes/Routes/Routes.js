a42be83ef412a36900a5b01025839c86b29e0b20
AmanatUMahmud <amanatmahmud@gmail.com>
2023-03-24 11:54:06 +0600

    route added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Error from "../../Pages/Error/Error";
 import Home from "../../Pages/Home/Home";
+import Addmoney from "../../Pages/Home/OurService/Addmoney/Addmoney";
 export const router = createBrowserRouter([
     {
         path: '/',
@@ -11,6 +12,26 @@ export const router = createBrowserRouter([
                 path: '/',
                 element: <Home/>
             },
+            {
+                path: '/addmoney',
+                element: <Addmoney/>
+            },
+            {
+                path: '/sendmoney',
+                element: <Home/>
+            },
+            {
+                path: '/payment',
+                element: <Home/>
+            },
+            {
+                path: '/paybill',
+                element: <Home/>
+            },
+            {
+                path: '/recharge',
+                element: <Home/>
+            }
             
 
         ],
